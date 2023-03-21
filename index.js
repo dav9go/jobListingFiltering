69eeee98dc9beca3f2a03288f2141d3d1262bfa7
dav9go <david9go@gmail.com>
2023-03-21 18:12:02 +0700

    search functionality added

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ function initialiceApp() {
     const data = await fetch("./data.json");
     const result = await data.json();
     console.log(result);
+    //Create all the components
     createComponent(result);
+    //Add clearbutton functionality
+    clearButton();
+    //Event listeners when adding or removing tags
+    addOrRemoveElementsFilterUpdate();
   });
 }
 
@@ -51,12 +56,20 @@ function createComponent(result) {
     const posted = document.createElement("p");
     posted.innerText = item.postedAt;
     infoWrapper.appendChild(posted);
+    const dot1 = document.createElement("p");
+    dot1.innerText = "•";
+    infoWrapper.appendChild(dot1);
     const contract = document.createElement("p");
     contract.innerText = item.contract;
     infoWrapper.appendChild(contract);
+    const dot2 = document.createElement("p");
+    dot2.innerText = "•";
+    infoWrapper.appendChild(dot2);
     const location = document.createElement("p");
     location.innerText = item.location;
     infoWrapper.appendChild(location);
+    const line = document.createElement("hr");
+    article.appendChild(line);
     const languagesWrapper = document.createElement("div");
     languagesWrapper.classList.add("languages-wrapper");
     article.appendChild(languagesWrapper);
@@ -86,5 +99,109 @@ function createComponent(result) {
     photo.classList.add("photo");
     photo.src = item.logo;
     article.appendChild(photo);
+  });
+  //Add filter functionality
+  filter();
+}
+
+let filterTagsArray = [];
+//Function that adds filter functionality and doesnt allow duplicates
+function filter() {
+  const tags = document.querySelectorAll(".language-tag");
+  tags.forEach((tag) => {
+    tag.addEventListener("click", function () {
+      if (!filterTagsArray.includes(tag.innerText)) {
+        filterTagsArray.push(tag.innerText);
+        const filterBox = document.querySelector(".filter");
+        const filterTag = document.createElement("div");
+        filterTag.classList.add("filter-tag");
+        filterTag.innerText = tag.innerText;
+        filterTag.addEventListener("click", function () {
+          console.log(filterTagsArray);
+          filterTagsArray = filterTagsArray.filter(
+            (item) => item !== filterTag.innerText
+          );
+          filterTag.remove();
+        });
+        filterBox.appendChild(filterTag);
+      }
+    });
+  });
+}
+
+//Event listener for the clear button
+function clearButton() {
+  const clearButton = document.querySelector(".clear");
+  clearButton.addEventListener("click", function () {
+    filterTagsArray = [];
+    const filterBox = document.querySelector(".filter");
+    filterBox.innerHTML = "";
+  });
+}
+
+function addOrRemoveElementsFilterUpdate() {
+  const filterBox = document.querySelector(".filter");
+  filterBox.addEventListener("DOMNodeInserted", async function () {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+    const data = await fetch("./data.json");
+    const result = await data.json();
+    const filteredResult = result.filter((item) => {
+      function checkLanguages() {
+        const lang = item.languages;
+        let count = 0;
+        lang.forEach((item) => {
+          if (filterTagsArray.includes(item)) {
+            count++;
+          }
+        });
+        if (count > 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      return (
+        filterTagsArray.includes(item.role) ||
+        filterTagsArray.includes(item.level) ||
+        checkLanguages() ||
+        filterTagsArray.includes(item.tools)
+      );
+    });
+    createComponent(filteredResult);
+  });
+
+  filterBox.addEventListener("DOMNodeRemoved", async function () {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+    const data = await fetch("./data.json");
+    const result = await data.json();
+    if (filterTagsArray.length > 0) {
+      const filteredResult = result.filter((item) => {
+        function checkLanguages() {
+          const lang = item.languages;
+          let count = 0;
+          lang.forEach((item) => {
+            if (filterTagsArray.includes(item)) {
+              count++;
+            }
+          });
+          if (count > 0) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+        return (
+          filterTagsArray.includes(item.role) ||
+          filterTagsArray.includes(item.level) ||
+          checkLanguages() ||
+          filterTagsArray.includes(item.tools)
+        );
+      });
+      createComponent(filteredResult);
+    } else {
+      createComponent(result);
+    }
   });
 }
